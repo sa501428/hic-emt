3c1d6204a72d51fa34a66d2469e9d7ee2680a141
Muhammad S Shamim <sa501428@gmail.com>
2022-02-07 16:56:47 -0600

    update printing

--- src/emt/utils/validation/VectorTools.java
+++ src/emt/utils/validation/VectorTools.java
@@ -1,5 +1,6 @@
 package emt.utils.validation;
 
+import emt.Globals;
 import javastraw.reader.datastructures.ListOfDoubleArrays;
 
 public class VectorTools {
@@ -12,17 +13,40 @@ public class VectorTools {
 
             long n = Math.min(data1.getLength(), data2.getLength());
 
+            double magnitude = 0;
+            double magnitude1 = 0;
+            double magnitude2 = 0;
             double absError = 0;
             for (long q = 0; q < n; q++) {
                 double err = Math.abs(data1.get(q) - data2.get(q));
+
+                if (!Double.isNaN(data1.get(q))) {
+                    magnitude1 += data1.get(q) * data1.get(q);
+                }
+
+                if (!Double.isNaN(data2.get(q))) {
+                    magnitude2 += data2.get(q) * data2.get(q);
+                }
+
                 if (!Double.isNaN(err)) {
+                    magnitude += data1.get(q) * data2.get(q);
                     absError += err;
                 }
             }
+            magnitude = Math.sqrt(magnitude);
+            magnitude1 = Math.sqrt(magnitude1);
+            magnitude2 = Math.sqrt(magnitude2);
 
             if (absError > 1e-10) {
                 System.err.println("Vector difference too big " + absError);
                 System.exit(25);
+            } else {
+                if (Globals.printVerboseComments) {
+                    System.err.println("Vector difference acceptable " + absError
+                            + " / " + magnitude
+                            + " / " + magnitude1
+                            + " / " + magnitude2);
+                }
             }
         } catch (Exception e) {
             //e.printStackTrace();
